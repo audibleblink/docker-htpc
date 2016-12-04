3e843008610be3837eb353011e684638bf949097
Alex Flores <domain@me.com>
2016-12-04 17:33:50 -0500

    initial upload

---
+++ portal/htportal/main.js
@@ -0,0 +1,11 @@
+// 'use strict';
+//
+// const sabLink    = getElementById('sab')
+// const sonarrLink = getElementById('sonarr')
+// const couchLink  = getElementById('couch')
+// const frame      = getElementById('content')
+//
+// sabLink.addEventListener('click', function (e) {
+//   e.preventDefault()
+//   frame.setAttribute('src', 
+// })
